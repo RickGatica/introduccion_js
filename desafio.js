7d64a0e5b8881e327db904af6962bf0d0fd74cc7
RickGatica <psychofreud@outlook.cl>
2022-09-13 16:02:42 -0300

    work done

---
+++ desafio.js
@@ -0,0 +1,40 @@
+/* Crea una lista con los siguientes elementos:
+
+- Tu nombre (string)
+- Tu apellido (string)
+- Tu edad (number)
+- ¿Eres desarrollador de aplicaciones web? (boolean)
+- Tu fecha de nacimiento (Date)
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+- Tu pasatiempos favoritos (Array)
+- Declara una funcion miActividadFavorita la cual retorne un string con la accion que mas te guste realizar */
+
+let name = "Rick";
+let lastName = "Gatica";
+let age = 29;
+let webDeveloper = true;
+
+let birthday = new Object();
+birthday.month = "November";
+birthday.day = 12;
+birthday.year = 1992;
+
+let myFavoriteBook = new Object();
+myFavoriteBook.tittle = "1984";
+myFavoriteBook.author = "George Orwell";
+myFavoriteBook.date = 1949;
+myFavoriteBook.url = "https://en.wikipedia.org/wiki/Nineteen_Eighty-Four";
+
+let hobbies = [
+  "Coding",
+  "Read Documentation",
+  "Play Video Games",
+  "Pair Programming with friends",
+  "Learn new technologies",
+  "Sing alone",
+  "Annoy my cat",
+];
+
+function miActividadFavorita() {
+  return "My favorite activity it's learn about logic behind programming";
+}
